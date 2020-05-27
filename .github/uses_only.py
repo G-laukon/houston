dd396bd36a6c6d70a815ebe9407aad63be344724
G_laukon <36465086+G-laukon@users.noreply.github.com>
2020-05-27 10:38:31 +0800

    Update uses_only.py

--- .github/uses_only.py
+++ .github/uses_only.py
@@ -1,10 +1,7 @@
 def uses_only(word,letters) :
     n = len(word)
-    m = 0
-    x = word[m]
     for x in word:
-        if letters.find(x) < 0:
-            return False
-        if m == n-1:
-            return letters.find(x) > -1  
-        m = m + 1
+        if letters.find(x) >= 0:
+            n = n - 1
+        if n == 0:
+            return True
