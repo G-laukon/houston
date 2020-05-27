d970233038b57a606a26bedfaaecc7b1f003c11a
G_laukon <36465086+G-laukon@users.noreply.github.com>
2020-05-27 10:20:26 +0800

    Update uses_all.py

--- .github/uses_all.py
+++ .github/uses_all.py
@@ -1,10 +1,7 @@
 def uses_all(word,letters) :
-    n = len(word)
-    m = 0
-    x = letters[m]
-    for x in letters:
+    n = len(letters)
+    for x in letters:   
         if word.find(x) > -1:
+            n = n - 1
+        if n == 0:
             return True
-        if m == n-1:
-            return word.find(x) > -1  
-        m = m + 1
