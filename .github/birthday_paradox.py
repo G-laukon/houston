a32ff0ec92df1f048db0cc9c7f9e5ab1379668c5
G_laukon <36465086+G-laukon@users.noreply.github.com>
2020-05-31 22:13:15 +0800

    Update birthday_paradox.py

--- .github/birthday_paradox.py
+++ .github/birthday_paradox.py
@@ -1,6 +1,19 @@
-def birthday_paradox(x):
+def birthday_paradox(samples):
+    birthday_co = 0
     import random
-    for in in range (x):
-    t = []
+    for i in range (samples):
+        team = []
         for i in range(23):
-            t.append(random.randint(1,365))
+            team.append(random.randint(1,365))
+        if has_duplicates(team) == True:
+            birthday_co += 1       
+    return birthday_co/samples
+    
+
+def has_duplicates(t):   
+    for x in t:
+        t_copy = t + []
+        t_copy.remove(x)
+        for y in t_copy :
+            if x == y:
+                return True
