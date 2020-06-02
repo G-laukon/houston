87363a9e4234136b895e226e342d5d89290fd948
G_laukon <36465086+G-laukon@users.noreply.github.com>
2020-06-02 13:29:11 +0800

    Update in_bisect.py

--- .github/in_bisect.py
+++ .github/in_bisect.py
@@ -5,23 +5,19 @@ def in_bisect(words,word):
         return None
     m = int(len(words_copy))
     n = m
-    while m > 1 :
-        x = len(words_copy)%2
-        if x == 0:
-            m = int(len(words_copy)/2)
-        if x == 1:
-            m = int((len(words_copy)+1)/2)
-            n = n + 1
-        print(m,'s')
-        word_m = words_copy[m]
-        if word_m > word:
+    while m > 1 :   
+        if len(words_copy)%2 == 1:
+            while words_copy[-1] < word:
+                return n
+            del words_copy[-1]
+            n = n - 1
+        m = int(len(words_copy)/2)
+        if words_copy[m] > word:
             words_copy = words_copy[:m]
             n = n - m
         else:
             words_copy = words_copy[m:]
-            n = n
-        print(words_copy)
+        print(m,words_copy)
     if words_copy[0] < word:
         return n
-    else:
-        return n -1
+    return n - 1
