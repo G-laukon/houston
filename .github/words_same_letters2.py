08e2f62aade57bed73fc258176c63644b095713f
G_laukon <36465086+G-laukon@users.noreply.github.com>
2020-06-13 17:52:18 +0800

    Update words_same_letters2.py

--- .github/words_same_letters2.py
+++ .github/words_same_letters2.py
@@ -8,17 +8,17 @@ def words_same_letters():
     s = dict()
     
     for word in fin:
-        t[word.strip()] = tuple(sorted(word.strip()))
+        t[word.strip()] = tuple(sorted(word.strip()))   #单词的字母生序排列生成元组储存为值
         
     fin.close()
     
-    for k in t:       
+    for k in t:      # k: key 
         if t[k] in s:
-            s[t[k]].append(k)
-        s.setdefault(t[k],[k])
+            s[t[k]].append(k)   # 合并相同键的值
+        s.setdefault(t[k],[k]) # 转换字典 t 的键和映射
         
     for m in s:
-        if len(s[m])> 1:
-            words_sld.setdefault(m,s[m])
-           
+        if len(s[m])> 1:        # 剔除没有异形词的项
+            words_sld.setdefault(m,s[m]) # for 语句内好像不能删除字典项，只能生成新的字典s
+
     return words_sld
