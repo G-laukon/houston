e33e00c3397bbd17a514299434b590704d354b5b
G_laukon <36465086+G-laukon@users.noreply.github.com>
2020-07-06 16:11:50 +0800

    Update analysis.py

--- .github/analysis.py
+++ .github/analysis.py
@@ -5,21 +5,27 @@ def anylise(filename,n):
     fin = open(str(filename))
     s = []
     hist = {}
-    val = {}
+    
     for line in fin:
         if line.startswith('*** START OF THIS'):
-            break       
+            break
+        
     for line in fin:
         if line.startswith('*** END OF THIS'):
             break       
         t = process_line(line)
-        u = s + t 
+        u = s + t
+        val = {}
         while len(u) > n:
-            key = tuple(u[:n])
-            val[u[n]] = val.setdefault(u[n],0)+ 1
-            hist[key] = val
+            key = tuple(u[:n]) 
+            if key in hist:
+                hist[key][u[n]] = hist[key].setdefault(u[n],0)+ 1
+            if key not in hist:
+                hist[key] = {u[n]:1}            
+            
             u = u[1:]
-        s = u               
+        s = u
+               
     fin.close()
     return hist
 
@@ -32,11 +38,13 @@ def process_line(line):
     line = line.split()      
     return line
 
+
 def main():
     filename = input('Filename:')
     n = int(input('Prefix words number:'))
     d = anylise(filename,n)
-    print(d)
+    d.sorted()
+    print
 
     
 if __name__ == '__main__':
